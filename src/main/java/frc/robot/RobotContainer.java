aac925886e407a4933ce648776ad6b3e8dd6cba5
Arin Bhandari <lightningminecraft75@gmail.com>
2026-03-06 15:58:21 -0700

    Added Logging

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,6 +4,8 @@
 
 package frc.robot;
 
+import org.littletonrobotics.junction.Logger;
+
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
